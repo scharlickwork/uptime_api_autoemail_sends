29390adb57b1f644a054ff4bdafe08bc2eef2d31
scharlick <68071772+scharlick@users.noreply.github.com>
2023-01-12 10:11:00 -0500

    working call, protected by send_to hard coded to scharlick

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from base64 import b64encode
 import requests
 import json
 import smtplib, ssl
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
 
 
 #global setting for email send
 smtp_server = 'smtp.gmail.com'
-gmail = 'noreply@cgps.org'
+gmail_user = 'noreply@cgps.org'
+username = gmail_user
 password = 'PwAzE8mK'
-smtp_port = 587
-
+smtp_port = 465
+sent_from = gmail_user
 
 
 def catch_them_all():
@@ -77,81 +75,30 @@ def catch_them_all():
     # for i in hasnotrestartedrecent:
     #     name.append((hasnotrestartedrecent[i]['computer name']).split('-')[0])
     #     up_time.append(hasnotrestartedrecent[i]['days up'])
+    with open('/Users/scharlick/PycharmProjects/uptime_api_autoemail_sends/api_call.txt', 'w') as fo:
+        fo.write(json.dumps(hasnotrestartedrecent))
     email_send(hasnotrestartedrecent)
 
+
 def email_send(hasnotrestartedrecent):
-    print(hasnotrestartedrecent)
-
-
-
-
-
-
-
-
-
+    number_of_computers = len(hasnotrestartedrecent)
+    for i in range(number_of_computers):
+        # print('days on: ' + hasnotrestartedrecent[i]['days up'])
+        #lints dict to get a email
+        # send_to_email_split = hasnotrestartedrecent[i]['computer name']
+        # send_to_email_split = send_to_email_split.split('-')
+        # send_to = send_to_email_split[0] + "@cgps.org"
+        send_to = 'scharlick@cgps.org'
+        up_time = hasnotrestartedrecent[i]['days up']
+        subject = 'Your computer may need a restart'
+        body = "Hello,\n\nThis is an automated email from the CGPS Tech Team. Our records indicate that your computer has been on for at least " + str(up_time) + " days. " \
+             "Computers start to have issues when they are left on for prolonged periods of time. " \
+             "To ensure your computer is running well, we strongly recommend that you restart your computer as soon as you can. If you have any questions please email us at support@cgps.org\n\n- CGPS Tech Team"
+        email_text = 'Subject: {}\n\n{}'.format(subject, body)
+        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+        server.ehlo()  # optional
+        server.login(username, password)
+        server.sendmail(sent_from, send_to, email_text)
+        server.close()
 
 catch_them_all()
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# #Replace with your own gmail account
-# cc = []
-# message = MIMEMultipart('mixed')
-# message['From'] = 'Contact <{sender}>'.format(sender = gmail)
-# message['To'] = student_email
-# message['Subject'] = 'Hello'
-# msg_content = '<h4>Hi There,<br> This is a testing message.</h4>\n'
-# body = MIMEText(msg_content, 'html')
-# message.attach(body)
-# with smtplib.SMTP(smtp_server, smtp_port) as server:
-#     server.ehlo()
-#     server.starttls(context=context)
-#     server.ehlo()
-#     server.login(gmail, password)
-#     server.sendmail(gmail, student_email.split(";") + (cc.split(";") if cc else []),
-#                 msg_full)
-#     server.quit()
-# print("email sent out successfully")
-#
-
-
-
-
-# #take the array and send emails
-# def inform_them_all(name, up_time):
-#    # Sets variables
-#    port = 465  # For SSL
-#    password = 'PwAzE8mK'
-#    username = 'noreply@cgps.org'
-#    gmail_user = 'noreply@cgps.org'
-#    # name = name
-#    # up_time = up_time
-#    sent_from = gmail_user
-#    trange = len(name)
-#    for i in range(trange):
-#       ##comma sperates multi addresses
-#       to = [name[i] + "@cgps.org"]
-#       subject = 'Your computer may need a restart'
-#       body = "Hello,\n\nThis is an automated email from the CGPS Tech Team. Our records indicate that your computer has been on for at least " + str(up_time[i]) + " days. " \
-#              "Computers start to have issues when they are left on for prolonged periods of time. " \
-#              "To ensure your computer is running well, we strongly recommend that you restart your computer as soon as you can. If you have any questions please email us at support@cgps.org\n\n- CGPS Tech Team"
-#
-#       email_text = 'Subject: {}\n\n{}'.format(subject, body)
-#
-#       server = smtplib.SMTP_SSL('smtp.gmail.com', port)
-#       server.ehlo()  # optional
-#       server.login(username, password)
-#       server.sendmail(sent_from, to, email_text)
-#       server.close()
